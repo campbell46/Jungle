1b245be762c978e9354d8ce59fad015c43ad6766
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-03-27 18:54:32 -0400

    Add home page tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+describe('Home Page', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  })
+  
+  it('There is at least 1 product on the page', () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it('There is 2 products on the page', () => {
+    cy.get(".products article").should("have.length", 2);
+  })
+
+})
+
